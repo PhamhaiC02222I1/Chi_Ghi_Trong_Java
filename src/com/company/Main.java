3adde0d5869c165e469d7868d37da23bfa2d765c
hai <humg.ola@gmail.com>
2022-03-25 11:24:11 +0700

    up1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Student test =new Student();
+    test.setName("Lu");
+    test.setClasses("C03");
+        System.out.println(test.getName());
+        System.out.println(test.getClasses());
+    }
+}
